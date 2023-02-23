e993308d86d17ff6a05e4af4243f5b6d7e3e7d9a
sabfiamo <sabfiamo@gmail.com>
2023-02-23 16:50:21 +0100

    add template html

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 console.log('>> Ready :)');
+//variables
+const COCKTAIL_NAME= '<manuelainclan>';
+
+const listCocktails = document.querySelector('.js-list-cocktails');
+
+const inputCoctailName = document.querySelector('.js-input');
+
+
+// const SERVER_URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${COCKTAIL_NAME}`;
+
+const COCKTAIL_URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`;
+
